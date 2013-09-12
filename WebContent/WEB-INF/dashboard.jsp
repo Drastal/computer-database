5a6e88acd0086f539ecfab150d207595d752445e
Drastal <drax91330@hotmail.fr>
2013-09-12 22:35:58 +0200

    Debut de pagination

--- WebContent/WEB-INF/dashboard.jsp
+++ WebContent/WEB-INF/dashboard.jsp
@@ -3,14 +3,13 @@
 <jsp:include page="include/header.jsp" />
 
 <section id="main">
-	<h1 id="homeTitle">${requestScope.sizeList} Computers found</h1>
+	<h1 id="homeTitle">${requestScope.sizeList}Computersfound</h1>
 	<div id="actions">
 		<form action="" method="GET">
 			<input type="search" id="searchbox" name="search" value=""
 				placeholder="Search name"> <input type="submit"
 				id="searchsubmit" value="Filter by name" class="btn primary">
 		</form>
-		<!-- where computer.name like -->
 		<a class="btn success" id="add" href="addComputer.aspx">Add
 			Computer</a>
 	</div>
@@ -60,6 +59,24 @@
 			</c:forEach>
 		</tbody>
 	</table>
+	<form action="" method="GET">
+		<div class="input" style="float: right;">
+			<label for="resultNb">Results per page: </label> <select
+				name="resultsNb">
+				<c:choose>
+					<c:when test="${requestScope.resultsQuantity==50}">
+						<option value="50" selected="selected">50</option>
+						<option value="100">100</option>
+					</c:when>
+					<c:otherwise>
+						<option value="50">50</option>
+						<option value="100" selected="selected">100</option>
+					</c:otherwise>
+				</c:choose>
+			</select> <input type="submit" value="Validate" class="btn primary">
+		</div>
+
+	</form>
 </section>
 
 <jsp:include page="include/footer.jsp" />
--- src/computer/database/controller/DashboardController.java
+++ src/computer/database/controller/DashboardController.java
@@ -18,6 +18,8 @@ import computer.database.service.manager.ServiceManager;
 @WebServlet("/computerList.aspx")
 public class DashboardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private int resultPerPage = 50;
+	private int totalPage = 1;
 
 	private DatabaseService machineService;
     /**
@@ -33,14 +35,22 @@ public class DashboardController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//Envoyer un objet dans la requete (ici la liste d'utilisateurs)
+		if(request.getParameter("resultsNb")!=null)
+			resultPerPage = Integer.parseInt(request.getParameter("resultsNb"));
+		//int pageNumber = Integer.parseInt(request.getParameter("pageNumber"));
+		request.setAttribute("resultsQuantity", resultPerPage);
 		String searching = request.getParameter("search");
 		if(searching==null||searching.trim().isEmpty()){
 			request.setAttribute("machines", machineService.getMachines());
 			request.setAttribute("sizeList", machineService.getMachines().size());
+			totalPage = (int)Math.ceil((double)machineService.getMachines().size() / resultPerPage);
 		} else {
 			request.setAttribute("machines", machineService.getMachines(searching));
 			request.setAttribute("sizeList", machineService.getMachines(searching).size());
+			totalPage = (int)Math.ceil((double)machineService.getMachines(searching).size() / resultPerPage);
 		}
+		System.out.println(totalPage);
+		System.out.println(resultPerPage);
 		
 		RequestDispatcher rd = getServletContext().getRequestDispatcher(response.encodeURL("/WEB-INF/dashboard.jsp"));// Ajout Web-inf ?
 		rd.forward(request, response);
